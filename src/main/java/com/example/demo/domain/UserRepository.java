414e5f7b1e62b3d34c9a0861782344b4664042b3
yzy <763117218@qq.com>
2019-02-19 11:43:34 +0800

    jpa

--- src/main/java/com/example/demo/domain/UserRepository.java
+++ src/main/java/com/example/demo/domain/UserRepository.java
@@ -13,4 +13,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface UserRepository extends JpaRepository<User, Long> {
 
+
+    User findByUserName(String userName);
+
+    User findByUserNameOrEmail(String userName, String email);
 }
